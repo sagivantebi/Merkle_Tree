8986740c1e1de22d340d443f135ac4628b4ca301
Sagiv Antebi <sagiva13@gmail.com>
2022-06-21 14:27:39 +0300

    All done except 6

--- merkle.py
+++ merkle.py
@@ -1,28 +1,18 @@
 # Sagiv Antebi, 318159282, Dvir Amram, 318192200
 # !/usr/bin/python3
+import binascii
 import hashlib
 
+from cryptography.exceptions import InvalidSignature
 from cryptography.hazmat.primitives import serialization
+from cryptography.hazmat.primitives.asymmetric import rsa
+from cryptography.hazmat.backends import default_backend
+from cryptography.hazmat.primitives import hashes
+from cryptography.hazmat.primitives.asymmetric import padding
 
 COUNT = [10]
 ID_NUM = 0
 
-from cryptography.hazmat.primitives.asymmetric import rsa
-from cryptography.hazmat.backends import default_backend
-
-private_key = rsa.generate_private_key(public_exponent=65537, key_size=2048, backend=default_backend())
-public_key = private_key.public_key()
-
-pem_puclic = public_key.public_bytes(encoding=serialization.Encoding.PEM,
-                                     format=serialization.PublicFormat.SubjectPublicKeyInfo)
-pem_private = private_key.private_bytes(encoding=serialization.Encoding.PEM,
-                                        format=serialization.PrivateFormat.TraditionalOpenSSL,
-                                        encryption_algorithm=serialization.NoEncryption())
-print(pem_private.decode())
-print(pem_puclic.decode())
-
-
-# print(pem.encode())
 
 class MerkleTreeNode:
     def __init__(self, value):
@@ -57,9 +47,6 @@ def build_tree(leaves):
             node2.parent = parent
             node1.neighbor = node2
             node2.neighbor = node1
-            # add 0 to left son and 1 to right son
-            node1.hashValue = '0' + node1.hashValue
-            node2.hashValue = '1' + node2.hashValue
             temp.append(parent)
         nodes = temp
     return nodes[0]
@@ -116,11 +103,47 @@ def findNodeByIdStr(root, listLeafs, idNode):
     return None
 
 
-def recursive_get_proof(root, nodeMerkle, str):
-    if root.value == nodeMerkle.value:
-        return str
-    str = str + " " + nodeMerkle.neighbor.hashValue
-    return recursive_get_proof(root, nodeMerkle.parent, str)
+def recursive_get_proof(nodeMerkle, str):
+    parent_node = nodeMerkle.parent
+    if parent_node is None:
+        return ""
+    else:
+        if parent_node.left.hashValue == str:
+            if parent_node.right is not None:
+                return "1" + parent_node.right.hashValue + " " + recursive_get_proof(parent_node, parent_node.hashValue)
+        else:
+            return "0" + parent_node.left.hashValue + " " + recursive_get_proof(parent_node, parent_node.hashValue)
+
+
+def sign(root, key):
+    key = serialization.load_pem_private_key(key.encode(), password=None)
+    message = root.hashValue.encode()
+    signature = key.sign(
+        message,
+        padding.PSS(
+            mgf=padding.MGF1(hashes.SHA256()),
+            salt_length=padding.PSS.MAX_LENGTH
+        ),
+        hashes.SHA256()
+    )
+    return binascii.b2a_base64(signature).decode()
+
+
+def checkValid(public_key, sig, text_verify):
+    public_key = serialization.load_pem_public_key(public_key.encode())
+    try:
+        public_key.verify(
+            binascii.a2b_base64(sig),
+            text_verify.encode(),
+            padding.PSS(
+                mgf=padding.MGF1(hashes.SHA256()),
+                salt_length=padding.PSS.MAX_LENGTH
+            ),
+            hashes.SHA256()
+        )
+    except InvalidSignature:
+        return False
+    return True
 
 
 def menu():
@@ -140,27 +163,59 @@ def menu():
             else:
                 print()
         elif firstChar == '3':
-            str = root.hashValue
             nodeMerkle = findNodeByIdInt(root, listLeafs, int(choice[2:]))
-            toPrint = recursive_get_proof(root, nodeMerkle, str)
-            print(toPrint)
+            toPrint = root.hashValue + " " + recursive_get_proof(nodeMerkle, nodeMerkle.hashValue)
+            print(toPrint[:-1])
         elif firstChar == '4':
             splitBySpaces = choice.split(" ", 2)
             value = splitBySpaces[1]
             inputCompare = splitBySpaces[2]
             nodeMerkle = findNodeByIdStr(root, listLeafs, value)
-            str = root.hashValue
-            toCompare = recursive_get_proof(root, nodeMerkle, str)
-            print(inputCompare == toCompare)
+            toCompare = root.hashValue + " " + recursive_get_proof(nodeMerkle, nodeMerkle.hashValue)
+            print(inputCompare == toCompare[:-1])
         elif firstChar == '5':
-            continue
+            private_key = rsa.generate_private_key(public_exponent=65537, key_size=2048, backend=default_backend())
+            public_key = private_key.public_key()
+
+            pem_public = public_key.public_bytes(encoding=serialization.Encoding.PEM,
+                                                 format=serialization.PublicFormat.SubjectPublicKeyInfo)
+            pem_private = private_key.private_bytes(encoding=serialization.Encoding.PEM,
+                                                    format=serialization.PrivateFormat.TraditionalOpenSSL,
+                                                    encryption_algorithm=serialization.NoEncryption())
+            print(pem_private.decode())
+            print(pem_public.decode())
+
         elif firstChar == '6':
-            continue
+            data = choice.split(" ")
+            # good check if the given text is by spaces
+            data_of_proof = ' '.join(data[1:]) + "\n"
+            temp = input()
+            while "-----END RSA PRIVATE KEY-----" not in temp:
+                data_of_proof = data_of_proof + temp + "\n"
+                temp = input()
+            data_of_proof = data_of_proof + temp
+            value_to_print = sign(root, data_of_proof)
+            print(value_to_print, end="")
+
         elif firstChar == '7':
-            continue
+            data = choice.split(" ")
+            # good check if the given text is by spaces
+            data_of_proof = ' '.join(data[1:]) + "\n"
+            temp = input()
+            while "-----END PUBLIC KEY-----" not in temp:
+                data_of_proof = data_of_proof + temp + "\n"
+                temp = input()
+            data_of_proof = data_of_proof + temp + "\n"
+            one_raw_to_dump = input()
+            root_signature = input().split(' ')
+            sig = root_signature[0]
+            text_verify = root_signature[1]
+            print(checkValid(data_of_proof, sig, text_verify))
         else:
             print("Invalid input")
         choice = input()
+        while len(choice) == 0:
+            choice = input()
         firstChar = choice[0]
 
 
